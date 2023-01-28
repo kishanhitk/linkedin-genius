7f78b7fe305ec14f1507b169b09faecb94d50025
kishanhitk <kishansharma1231@gmail.com>
2023-01-28 11:30:40 +0530

    feat: ui

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -51,7 +51,7 @@ export const Generator = () => {
         <h2 className="text-center text-2xl">LinkedIn Genius</h2>
         <h3 className="my-10 text-center text-lg font-light">
           Unlock the power of AI to showcase your achievements on LinkedIn with
-          <span className="px-1 font-normal  underline decoration-blue-300 underline-offset-4">
+          <span className="px-1 font-normal  underline decoration-blue-300 decoration-wavy underline-offset-2">
             LinkedIn Genius
           </span>
         </h3>
