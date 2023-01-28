4ac75602f5ebf99500894a572acc4133b2ce2e4e
kishanhitk <kishansharma1231@gmail.com>
2023-01-28 12:51:11 +0530

    feat: add back runtime config

--- src/pages/api/generatenew.ts
+++ src/pages/api/generatenew.ts
@@ -1,6 +1,10 @@
 import { OpenAIStream, OpenAIStreamPayload } from "@/utils/OpenAPIStream";
 import { NextRequest } from "next/server";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default async function handler(req: NextRequest) {
   const { tone, contentInput } = (await req.json()) as {
     tone: string;
